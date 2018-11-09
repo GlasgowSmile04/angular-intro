1816a079924e1773132605f95efde1b53d8f58f0
GlasgowSmile04 <zacglasgow@gmail.com>
2018-11-08 20:53:49 -0800

    Learned how to make a new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title: String = 'Hello World';
-  name: String = 'Zac';
-  currentDay: any = new Date().getDay();
-
-  days: Object = {
-    0: 'Sunday',
-    1: 'Monday',
-    2: 'Tuesday',
-    3: 'Wednesday',
-    4: 'Thursday',
-    5: 'Friday',
-    6: 'Saturday',
-  };
-
-  renderThisDay() {
-    return this.days[this.currentDay];
-  }
 
 }
