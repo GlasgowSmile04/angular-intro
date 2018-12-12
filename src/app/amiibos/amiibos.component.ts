6fc14133b8d680cc88c6cca10b5640f1a4b83e85
GlasgowSmile04 <zacglasgow@gmail.com>
2018-12-11 20:56:00 -0800

    Pagination and search components for amiibos area

--- src/app/amiibos/amiibos.component.ts
+++ src/app/amiibos/amiibos.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { AmiiboService } from '../services/amiibo.service';
 import { AmiiboInterface } from '../interfaces/amiibo-interface';
+import { PaginationService } from '../services/pagination.service';
 
 @Component({
   selector: 'app-amiibos',
@@ -10,15 +11,28 @@ import { AmiiboInterface } from '../interfaces/amiibo-interface';
 })
 export class AmiibosComponent implements OnInit {
 
-  amiibos;
+  amiibos: AmiiboInterface[];
+  paginator: any = {};
+  searchText: string;
+  pagedItems: AmiiboInterface[] = [];
 
-  constructor( private amiibo: AmiiboService ) { }
+  constructor( private amiibo: AmiiboService, private paginationService: PaginationService ) { }
 
   getAmiibos() {
     return this.amiibo.getAmiibos().subscribe((res => {
       console.log(res);
       this.amiibos = res.amiibo;
+      this.setPage(1);
     }));
+  }
+
+  setPage(page: number) {
+    const amiiboCount = this.amiibos.length;
+    this.paginator = this.paginationService.getPaginator(amiiboCount, page, 12);
+    if ( page < 1 || page > this.paginator.pagesCount ) { return; }
+    const startIndex = this.paginator.startIndex,
+          endIndex = this.paginator.endIndex + 1;
+    this.pagedItems = this.amiibos.slice(startIndex, endIndex);
   }
 
   ngOnInit() {
@@ -26,3 +40,4 @@ export class AmiibosComponent implements OnInit {
   }
 
 }
+
